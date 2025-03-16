c6fa746dbb8075adc4ce3954309df5c856f2b274
Gultaj Muradova 14125 <gultajmuradova14125@ada.edu.az>
2025-03-17 00:46:37 +0400

    add admin panel spring boot app

---
+++ signTalks-admin/src/main/java/az/edu/ada/adminpanel/admin/SignTalksAdminPanelApplication.java
@@ -0,0 +1,13 @@
+package az.edu.ada.adminpanel.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignTalksAdminPanelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignTalksAdminPanelApplication.class, args);
+	}
+
+}
